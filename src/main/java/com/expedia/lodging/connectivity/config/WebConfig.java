b53766e123373a15c3e14cb9cedad0e0df18729a
fuzantott@163.com <fuzan19841022>
2014-09-30 09:45:53 -0700

    resource handler.

--- src/main/java/com/expedia/lodging/connectivity/config/WebConfig.java
+++ src/main/java/com/expedia/lodging/connectivity/config/WebConfig.java
@@ -3,28 +3,20 @@ package com.expedia.lodging.connectivity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-import com.expedia.lodging.connectivity.service.MessageService;
 
 @Configuration
 @ComponentScan("com.expedia.lodging.connectivity")
 @EnableWebMvc
 @EnableTransactionManagement
-<<<<<<< Updated upstream
 @Import({BeanConfig.class})
-public class WebConfig {
-=======
-//@Import({BeanConfig.class})
 public class WebConfig extends WebMvcConfigurerAdapter{
->>>>>>> Stashed changes
-	
-	
-	
 	
 	 @Override
 	 public void addResourceHandlers(ResourceHandlerRegistry registry) {
