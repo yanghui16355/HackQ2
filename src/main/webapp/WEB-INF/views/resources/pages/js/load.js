81f24986dc1fda4962d0b44abcb8dedc4cbefddb
fuzantott@163.com <fuzan19841022>
2014-09-30 16:10:47 -0700

    local commit

--- src/main/webapp/WEB-INF/views/resources/pages/js/load.js
+++ src/main/webapp/WEB-INF/views/resources/pages/js/load.js
@@ -1,70 +1,9 @@
-$(document).ready(function() {
-<<<<<<< HEAD
-	//eventListensers($("div.container"));
-=======
-	
->>>>>>> 2b667872261b38d219d4acb525d7c7573f319f8b
-	//$('#progress1').attr('style',"width: 37%");
-	//$('#progress1').css({
-	// 'background-color': '#66FF66'
-	//});
-<<<<<<< HEAD
-=======
-	console.log($(".btn.btn-primary.btn-lg"));
-	loadD();
->>>>>>> 2b667872261b38d219d4acb525d7c7573f319f8b
-	updateMessages();
-});
-
-function poll() {
-	setTimeout(updateMessages(), 5000);
-}
-
 function eventListensers(object) {
 	object.onload(function() {
 		console.log('Test!');
 	}, false);
 }
 
-<<<<<<< HEAD
-function loadD(args){
-	for(var k in $("a.btn btn-primary btn-lg")){
-		k.click(function(e){
-			e.preventDefault();
-			call(k.attr("ID"));
-			k.click();
-		});
-	}
-=======
-function loadD(){
-	
-//	for(var k in $(".btn.btn-primary.btn-lg")){
-	//	console.log(k);
-	console.log(".btn.btn-primary.btn-lg");
-	$(".btn.btn-primary.btn-lg").click(function(e){
-//		k.click(function(e){
-			e.preventDefault();
-		//	call(k.attr("ID"));
-			var data = getPartnerDetails("Hilton");
-		//	$(".btn.btn-primary.btn-lg").click()(function(){
-			console.log($('#myModalLabel'));
-			console.log(data);
-			console.log(data.partnerID);
-			$('#myModalLabel').html(data.partnerID+": "+data.rqinS.category);
-			$("#RQIn").children(".message").html("Source: "+data.rqinS.sourceID+"</br>"+"Destinatoin: "+data.rqinS.destinationID
-					+"</br>"+"Success: "+data.rqinS.amount+"</br>"+"Failure: "+data.rqinF.amount);
-			$("#RQOut").children(".message").html("Source: "+data.rqoutS.sourceID+"</br>"+"Destinatoin: "+data.rqoutS.destinationID
-					+"</br>"+"Success: "+data.rqoutS.amount+"</br>"+"Failure: "+data.rqoutF.amount);
-			$("#RSIn").children(".message").html("Source: "+data.rsinS.sourceID+"</br>"+"Destinatoin: "+data.rsinS.destinationID
-					+"</br>"+"Success: "+data.rsinS.amount+"</br>"+"Failure: "+data.rsinF.amount);
-			$("#RSOut").children(".message").html("Source: "+data.rsoutS.sourceID+"</br>"+"Destinatoin: "+data.rsoutS.destinationID
-					+"</br>"+"Success: "+data.rsoutS.amount+"</br>"+"Failure: "+data.rsoutF.amount);
-		//	});
-		});
-	//}
->>>>>>> 2b667872261b38d219d4acb525d7c7573f319f8b
-	
-}
 
 /**
  *
@@ -83,15 +22,11 @@ function renderPage(json) {
  * 
  * @param {Object} hotels
  */
-function createBars(hotels) {
-	for (var hotel in hotels) {
-		console.log(hotel, result[hotel]);
-
-		for (var message in result[hotel]) {
-			console.log(message);
-			createBar(message);
-		}
-
+function analyzeData(hotels,len_of_json) {
+	for( var i = 0; i< len_of_json ;i++){
+		//for (var hotel in hotels[i]) {
+			createBars(hotels[i],i);			
+		//}
 	}
 }
 
@@ -105,79 +40,166 @@ function removeBars(bar) {
 		});
 }
 
-function createBar(message) {
+/**
+ * 
+ * 
+ * 
+ */
+function createBars(message,counter) {
 	
-	// <p><a class="btn btn-primary btn-lg" role="button" data-toggle="modal" data-target="#myModal" id="ParterID">
-        	// Expedia.com ---------> Hilton ---------> Expedia.com &raquo;</a></p>
-        	
-    $('<p/>');
+	var identity = message["partnerID"] +"_"+ "Expedia.com";
+	var id = 0;
+	if( counter % 2 == 0 ){
+		//$('<div/>',{'class':'container','id':identity+"_container_div"+counter}).insertAfter("#main_jumbotorn") ;
+		id = identity+"_container_div_"+counter;
+		$('<div class="container" id="'+id+'">111222</div>').insertAfter("#main_jumbotorn") ;
+	}else{
+		//$('<div/>',{'class':'jumbotron','id':identity+"_jumbotron_div"+counter}).insertAfter("#main_jumbotorn") ;
+		//$('<div/>',{'class':'container','id':identity+"_container_div"+counter}).appendTo("#"+identity+"_jumbotron_div") ;
+		
+		var temp_id = identity+"_container_jum_div_"+counter;
+		id = identity+"_container_div_"+counter;
+		$('<div class="jumbotron" id="'+temp_id+'">567</div>').insertAfter("#main_jumbotorn") ;
+		
+		console.log($("#"+temp_id));
+		$('<div class="container" id="'+id+'">890wwwww</div>').append("#"+temp_id) ;
+
+	}
+	var i = 0;
+	for(var key in message){
+		createOneBar(id, message[key] ,identity ,i);
+		i++;
+	}   	 	
+}
+
+
+function createOneBar(divId,message,_identity,counter){
+	var identity = message["sourceID"] + "_" + message["destinationID"];
+	
+	
+	//$('<p/>',{'id':identity + "_" +message['type'] +'_p'}).appendTo("#"+divId);
+    $('<p><a class="btn btn-primary btn-lg" role="button" data-toggle="modal" data-target="#myModal" id="ParterID"></a></p>').appendTo("#"+divId);
     
-    $('<a/>',{'class':''});
+    $('<a/>',{'class':'btn btn-primary btn-lg',
+    'role':'button',
+    'data-toggle' : 'modal',
+    'data-target' : '#myModal',
+    'id':identity + "_" +message['type'] +'_a'
+    }).appendTo("#"+ identity + "_" +message['type'] +'_p');
 	
+	
+	console.log("++++++++++++++++++++++++++++++++++++++++++++");
 	
 	$('<div/>', {
 		'class' : 'progress',
-		'id' : 'progress_test_1'
-	}).appendTo("#container2");
+		'id' : 'progress_'+identity+"_"+message['type']
+	}).after("#"+identity + "_" +message['type'] +'_p');
 
 	$('<div/>', {
 		'class' : 'progress-bar progress-bar-success',
 		'style' : 'width: 35%',
-		'id' : 'progress_test1'
-	}).appendTo("#progress_test_1");
+		'id' : 'progress_'+counter+'_' + message['type'] +identity+ "_bar",
+		'data-toggle':"tooltip",
+		'data-placement':"top",
+		'title':'show test title'
+	}).appendTo('#progress_'+identity+"_"+message['type']);
 
 	$('<span/>', {
-		'innerHTML' : '35% Complete (success)'
-	}).appendTo("#progress_test1");
+		'innerHTML' : 'test data '
+	}).appendTo('#progress_'+counter+'_' + message['type'] +identity+ "_bar");
 	
+
+	$('#progress_'+identity+"_"+message['type']).tooltip();
 	
-	
-	$('#progress2').tooltip();
-	
-	
-	$('#progress1').css({
-	 'background-color': '#66FF66'
+	$('progress_'+counter+'_' + message['type'] +identity+ "_bar").css({
+	 'background-color': message["color"]
 	});
 }
 
 function updateMessages() {
 	var response = $.ajax({
 		type : "GET",
-<<<<<<< HEAD
 		url : "/HackQ2-0.0.1/api/partnerInfo",
-=======
-		url : "/hackq2/api/partnerInfo",
->>>>>>> 2b667872261b38d219d4acb525d7c7573f319f8b
 		dataType : "json",
 		async : false,
 		cache : false,
-		/*complete : poll,*/
 		timeout : 5000
 	}).responseText;
-	//console.log(response);
 	var data = jQuery.parseJSON(response);
-	console.log(data[0]);
-	for( var k in data[0] ){
-		console.log(k);
-	}
 	return data;
 }
 
-function getPartnerDetails(partnerID) {
-	var response = $.ajax({
-		type : "GET",
-		url : "/hackq2/api/partnerInfo/"+partnerID,
-		dataType : "json",
-		async : false,
-		cache : false,
-		/*complete : poll,*/
-	}).responseText;
-	//console.log(response);
-	var partner = jQuery.parseJSON(response);
-	console.log(partner);
-	console.log(partner.partnerID);
-	for( var k in partner){
-		console.log(k);
-	}
-	return partner;
+
+function getContainerTemplate(args){
+	
+	var content = 
+	   '<div class="container">'+
+			'<p><a class="btn btn-primary btn-lg" role="button" data-toggle="modal" data-target="#myModal" id="'+args["id"]+'"></a></p>'+
+			'<div class="progress">'+
+				'<div id="progress1" class="progress-bar progress-bar-success" style="width: '+args[width]+'%">'+
+					'<span>'+args[width]+'% Complete (success)</span>'+
+				'</div>'+
+				'<div class="progress-bar progress-bar-success" style="width: '+args[width]+'%">'+
+					'<span>20% Complete (warning)</span>'+
+				'</div>'+
+				'<div id="progress2" class="progress-bar progress-bar-success" data-toggle="tooltip" data-placement="top" title="Tooltip on left" class="progress-bar progress-bar-danger" style="width: 45%">'+
+					'<span>45% Complete (danger)</span>'+
+				'</div>'+
+				'<div class="progress-bar progress-bar-success" style="width: '+args[width]+'%">'+
+					'<span>20% Complete (warning)</span>'+
+				'</div>'+
+				'<div class="progress-bar progress-bar-success" style="width: '+args[width]+'%">'+
+					'<span>20% Complete (warning)</span>'+
+				'</div>'+
+				'<div class="progress-bar progress-bar-success" style="width: '+args[width]+'%">'+
+					'<span>20% Complete (warning)</span>'+
+				'</div>'+
+				'<div class="progress-bar progress-bar-success" style="width: '+args[width]+'%">'+
+					'<span>20% Complete (warning)</span>'+
+				'</div>'+
+				'<div class="progress-bar progress-bar-success" style="width: '+args[width]+'%">'+
+					'<span>20% Complete (warning)</span>'+
+				'</div>'+
+			'</div>'+
+		'</div>';
+	return content;
 }
+
+function getJumContainerTemplate(args){
+	var content = 
+	   '<div class="container">'+
+			'<p><a class="btn btn-primary btn-lg" role="button" data-toggle="modal" data-target="#myModal" id="'+args["id"]+'"></a></p>'+
+			'<div class="progress">'+
+				'<div id="progress1" class="progress-bar progress-bar-success" style="width: '+args[width]+'%">'+
+					'<span>'+args[width]+'% Complete (success)</span>'+
+				'</div>'+
+				'<div class="progress-bar progress-bar-success" style="width: '+args[width]+'%">'+
+					'<span>20% Complete (warning)</span>'+
+				'</div>'+
+				'<div id="progress2" class="progress-bar progress-bar-success" data-toggle="tooltip" data-placement="top" title="Tooltip on left" class="progress-bar progress-bar-danger" style="width: 45%">'+
+					'<span>45% Complete (danger)</span>'+
+				'</div>'+
+				'<div class="progress-bar progress-bar-success" style="width: '+args[width]+'%">'+
+					'<span>20% Complete (warning)</span>'+
+				'</div>'+
+				'<div class="progress-bar progress-bar-success" style="width: '+args[width]+'%">'+
+					'<span>20% Complete (warning)</span>'+
+				'</div>'+
+				'<div class="progress-bar progress-bar-success" style="width: '+args[width]+'%">'+
+					'<span>20% Complete (warning)</span>'+
+				'</div>'+
+				'<div class="progress-bar progress-bar-success" style="width: '+args[width]+'%">'+
+					'<span>20% Complete (warning)</span>'+
+				'</div>'+
+				'<div class="progress-bar progress-bar-success" style="width: '+args[width]+'%">'+
+					'<span>20% Complete (warning)</span>'+
+				'</div>'+
+			'</div>'+
+		'</div>';
+	return content;
+}
+
+$(document).ready(function() {
+	data = updateMessages();
+	analyzeData(data,data.length);
+});
